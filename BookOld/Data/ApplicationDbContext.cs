03d70f610cea48ea67c844d65983c00b81973f6a
kourosh karimi <karimikorosh2000@gmail.com>
2024-02-03 22:15:00 +0330

    CRUD pt.1

---
+++ BookOld/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookOld.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookOld.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Category> Categories { get; set; }
+}
