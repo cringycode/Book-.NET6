dff010279a3f58962f4fc4af276bb003e7d15131
kourosh karimi <karimikorosh2000@gmail.com>
2024-02-05 01:09:19 +0330

    CRUD pt.2

---
+++ BookOld/Controllers/CategoryController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookOld.Data;
+using BookOld.Models;
+
+namespace BookOld.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        IEnumerable<Category> objCategoryList = _db.Categories;
+        return View(objCategoryList);
+    }
+
+    public IActionResult Create()
+    {
+        IEnumerable<Category> objCategoryList = _db.Categories;
+        return View();
+    }
+
+    [HttpPost]
+    [AutoValidateAntiforgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(obj);
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var categoryFromDb = _db.Categories.Find(id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(categoryFromDb);
+    }
+
+    [HttpPost]
+    [AutoValidateAntiforgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(obj);
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var categoryFromDb = _db.Categories.Find(id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(categoryFromDb);
+    }
+
+    [HttpPost]
+    [AutoValidateAntiforgeryToken]
+    public IActionResult DeletePost(int? id)
+    {
+        var obj = _db.Categories.Find(id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        return RedirectToAction(nameof(Index));
+
+        return View(obj);
+    }
+}
--- BookOld/Models/Category.cs
+++ BookOld/Models/Category.cs
@@ -1,13 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookOld.Models;
 
 public class Category
 {
     [Key] public int Id { get; set; }
-
     [Required] public string Name { get; set; }
 
+    [DisplayName("Display Order")]
+    [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
     public int DisplayOrder { get; set; }
+
     public DateTime CreateDateTime { get; set; } = DateTime.Now;
 }
