e682082c80a08c168fc322eac91a397fdf74ddd9
kourosh karimi <karimikorosh2000@gmail.com>
2024-02-29 17:53:39 +0330

    Order Management pt.1

---
+++ Book.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,7 @@
+﻿namespace Book.Models.ViewModels;
+
+public class OrderVM
+{
+    public OrderHeader OrderHeader { get; set; }
+    public IEnumerable<OrderDetail> OrderDetail { get; set; }
+}
---
+++ BookOld/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,81 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Book.DataAccess.Repository.IRepository;
+using Book.Models;
+using Book.Models.ViewModels;
+using Book.Utility;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BookOld.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        [BindProperty] public OrderVM OrderVM { get; set; }
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int orderId)
+        {
+            OrderVM = new OrderVM()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault
+                    (u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll
+                    (u => u.OrderId == orderId, includeProperties: "Product")
+            };
+
+            return View(OrderVM);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+
+            if (User.IsInRole(SD.Role_Admin))
+            {
+                orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                var claimIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderHeaders = _unitOfWork.OrderHeader.GetAll
+                    (u => u.ApplicationUserId == claim.Value, includeProperties: "ApplicationUser");
+            }
+
+            switch (status)
+            {
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+    }
+}
